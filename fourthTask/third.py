aea8f571eac7752588b60925fd914083bb3bb3f7
nicka-CC <128600081+nicka-CC@users.noreply.github.com>
2024-05-04 22:46:43 +0300

    fourthTask

--- fourthTask/third.py
+++ fourthTask/third.py
@@ -1,10 +1,13 @@
 def zapisivatel(text: str, filename: str):
-    with open(filename, 'a+',encoding='utf-8') as file:
+    with open(filename, 'a+',encoding='utf-8')as file:
         file.write(text + '\n')  
         file.seek(0)  
         lines = file.readlines()
-    for index, line in enumerate(lines):
-        if index % 2 == 1: 
-            print(line.strip())  
-zapisivatel("Пример текста для записи в файл", "./test.txt")
-zapisivatel("Еще одна строка текста", "./test.txt")
+        if len(lines) % 2 == 0:
+            print(text)
+      
+zapisivatel("Первый раз", "./fourthTask/test.txt")
+zapisivatel("Второй раз", "./fourthTask/test.txt")
+zapisivatel("Третий раз", "./fourthTask/test.txt")
+zapisivatel("Четвертый раз", "./fourthTask/test.txt")
+zapisivatel("Пятый раз", "./fourthTask/test.txt")
